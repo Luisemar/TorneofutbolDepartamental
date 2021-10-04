bbfc69462836308a166044575126237cd8a15d14
addgp <andresgonza2004@gmail.com>
2021-10-04 12:00:39 -0500

    primer commit

--- TorneoFutbolDptl.App.Persistencia/Migrations/20211004165259_Entidades.Designer.cs
+++ TorneoFutbolDptl.App.Persistencia/Migrations/20211004165259_Entidades.Designer.cs
@@ -10,7 +10,7 @@ using TorneoFutbolDptl.App.Persistencia;
 namespace TorneoFutbolDptl.App.Persistencia.Migrations
 {
     [DbContext(typeof(AppContext))]
-    [Migration("20210927172135_Entidades")]
+    [Migration("20211004165259_Entidades")]
     partial class Entidades
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
