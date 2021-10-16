79c51b32f233c03ec6252c9716649ed23713a4a6
Mahas <mahamantradas@hotmail.com>
2021-10-16 15:52:05 +0200

    Nombres

--- TorneoFutbolDptl.App.Dominio/Entidades/Equipo.cs
+++ TorneoFutbolDptl.App.Dominio/Entidades/Equipo.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System;
 using System.ComponentModel.DataAnnotations;
 namespace TorneoFutbolDptl.App.Dominio
@@ -7,10 +6,15 @@ namespace TorneoFutbolDptl.App.Dominio
     {
         // Identificador único de cada Equipo
         public int Id { get; set; }
+
+        [Display(Name = "Nombre del Equipo: ")]
+    
         public string Nombre { get; set; }
         // Relacion entre Equipo y el municipio Fk
+        [Display(Name = "Municipio del Equipo: ")]
         public Municipio Municipio {get; set;}
         // Relacion entre Equipo y DT  Fk
+        [Display(Name = "DT del equipo")]
         public DirectorTecnico DirectorTecnico { get; set; }
     }
 }
